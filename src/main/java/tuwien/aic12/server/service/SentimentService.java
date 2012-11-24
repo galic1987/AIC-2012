d6d8d78ad20decb65edcd811b4b9b9347c39c4c3
galic1987 <galic1987@gmail.com>
2012-11-24 17:48:53 +0100

    added jobDto and sentimentservice

--- src/main/java/tuwien/aic12/server/service/SentimentService.java
+++ src/main/java/tuwien/aic12/server/service/SentimentService.java
@@ -3,11 +3,13 @@ package tuwien.aic12.server.service;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 import tuwien.aic12.model.Customer;
+import tuwien.aic12.dto.JobDTO;
+
 
 @WebService(name="sentimentService")
 public interface SentimentService {
 
-	public String startAnaylsis(@WebParam(name = "companyName") String companyName);
+	public String startAnaylsis(@WebParam(name = "job") JobDTO job);
         
         
 	

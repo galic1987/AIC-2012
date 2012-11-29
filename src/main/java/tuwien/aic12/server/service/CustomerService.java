89f92af60c3aa2baa0f6f84ef2de12597094ce41
vanjalee <vanjalee@vanjalee>
2012-11-29 02:53:28 +0100

    Now performing sentiment analysis in thread and sends an email to the user upon finish

--- src/main/java/tuwien/aic12/server/service/CustomerService.java
+++ src/main/java/tuwien/aic12/server/service/CustomerService.java
@@ -3,10 +3,10 @@ package tuwien.aic12.server.service;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
-@WebService//(name = "CustomerService", portName="CustomerServicePort")
+@WebService
 public interface CustomerService {
 
-    public String registerCustomer(@WebParam(name = "username") String username, @WebParam(name = "password") String password);
+    public String registerCustomer(@WebParam(name = "username") String username, @WebParam(name = "password") String password, @WebParam(name = "companyname") String companyname);
 
     public String login(@WebParam(name = "username") String username, @WebParam(name = "password") String password);
 

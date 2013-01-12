67b1352d130cb4858d31b08a44da921ee10c3094
hp <hp@7100mt>
2013-01-12 22:13:26 +0100

    will be explained in email

--- src/main/java/tuwien/aic12/server/service/BillingService.java
+++ src/main/java/tuwien/aic12/server/service/BillingService.java
@@ -11,7 +11,7 @@ public interface BillingService {
     public String getSingleJobBill(@WebParam(name = "jobId") Long jobId, 
             @WebParam(name = "token") String token);
 
-    public String payRating(@WebParam(name = "ratingId") Long ratingId, 
+    public String payRating(@WebParam(name = "jobId") Long jobId, 
             @WebParam(name = "amount") Double amount);
 
     public String payBill(@WebParam(name = "amount") Double amount, 
